d86c34731a3496bace14c02c9f6d04987f86733b
nexia23 <karnetzki.maxim@gmx.de>
2024-02-16 15:58:03 +0100

    explicit keys for est save, tried new estimation parameter

--- Estimation/run_estimation.py
+++ Estimation/run_estimation.py
@@ -11,35 +11,57 @@ import json
 def get_params_bounds():
     bounds = OrderedDict({
     #'Hkt_init': (0.35, 0.55, False),   
-    'k_E_infect': (7e-7 , 4.5e-6, True),    #jetzt paramscan früher (1e-8 , 1e-4),
-    'tropism': (2, 200, False),
-    'M':  (1e2, 5e3, True),
-    'a_P_d': (15.6e1, 1e7, True),     
-    'k_P_d':  (1e-3, 10, True),      
-    'r_P_d': (4, 15, False),
-    'fac_R_d': (1e-10, 1, False),
-    'k_P_art_max': (1e-9, 1e1, True),
+    'k_E_infect': (7e-7 , 4.5e-6, True),    # jetzt paramscan früher (1e-8 , 1e-4),
+    #'tropism': (2, 200, False),
+    #'M':  (1e2, 5e3, True),
+    #'a_P_d': (15.6e1, 1e7, True),        
+    #'k_P_d':  (1e-3, 10, True),      
+    #'r_P_d': (4, 15, False),
+    #'fac_R_d': (1e-16, 1, False),
+    #'k_P_art_max': (1e-9, 1e1, True),
+    #'t_mat_P': (5,14, False),
     #'k_M_death': (60, 100, False),
     #'t_E_death': (100, 130, False),
-    #'k_iE_pit_frac': (0, 1, False),            # Anteil der iE die durch ART gepitted werden, 0-1. Rest sterben durch ART
+    #'k_iE_pit_frac': (0, 1, False),        # Anteil der iE die durch ART gepitted werden, 0-1. Rest sterben durch ART
     #'k_iE_art_max': (1, 1000),             # maximale abtötrate von iE durch ART, #parameterscan zu unsensibel 
-    #'h_art' :(3, 6, False),                 # assume similar to in vitro: [1] R. K. Tyagi u. a., doi: 10.1186/s12916-018-1156-x.
-    #'ID50': (1e-1, 1000),               #ART dosis bei der 50% der iE getötet werden, #parameterscan zu unsensibel       
-    #'pre_t': (2,6, True),            # time of ART addition, 3 and 5 in medians in data for non-pth and pth respectively
+    #'h_art' :(3, 6, False),                # assume similar to in vitro: [1] R. K. Tyagi u. a., doi: 10.1186/s12916-018-1156-x.
+    #'ID50': (1e-1, 1000),                  # ART dosis bei der 50% der iE getötet werden, #parameterscan zu unsensibel       
+    #'pre_t': (2,6, True),                  # time of ART addition, 3 and 5 in medians in data for non-pth and pth respectively
+    
     # Pth specific parameteres
-    's_BH_pth': (1e-9, 1e-5, True),      # slope of linear function defining bystander heamolysis strength
-    'LDH_pth': (140, 280, False),       # LDH concentration in blood plasma
+    'Hkt_init_pth': (0.35, 0.55, False),   
+    't_mat_P_pth': (5,14, False),
+    'tropism_pth': (2, 200, False),
+    'k_P_art_max_pth': (1e-9, 1e1, True),
+    't_E_death_pth': (40, 130, False),
+
+    's_BH_pth': (1e-9, 1e-5, True),         # slope of linear function defining bystander heamolysis strength
+    'LDH_pth': (140, 280, False),           # LDH concentration in blood plasma
     'k_M_death_pth': (30, 100, False),
-    't_E_death_pth': (80, 130, False),
-    #'Hkt_init_pth': (0.35, 0.55, False),   
-   
+    'M_pth':  (1e2, 5e3, True),
+    'a_P_d_pth': (15.6e1, 1e7, True),        
+    'k_P_d_pth':  (1e-3, 10, True),      
+    'r_P_d_pth': (4, 15, False),
+    'fac_R_d_pth': (1e-16, 1, False),
+    'k_iE_pit_frac_pth': (0, 1, False),        # Anteil der iE die durch ART gepitted werden, 0-1. Rest sterben durch ART
+
     # non-Pth specific parameteres
+    'Hkt_init_non': (0.35, 0.55, False),  
+    't_mat_P_non': (5,14, False),
+    'tropism_non': (2, 200, False),
+    'k_P_art_max_non': (1e-9, 1e1, True),
+    't_E_death_non': (40, 130, False),
+
     's_BH_non': (1e-11, 1e-8, True),      # slope of linear function defining bystander heamolysis strength
     'LDH_non': (140, 280, False),       # LDH concentration in blood plasma
     'k_M_death_non': (30, 100, False),
-    't_E_death_non': (80, 130, False),
-    #'Hkt_init_non': (0.35, 0.55, False),   
-
+    #'t_E_death_inf_non': (40, 130, False),
+    'M_non':  (1e2, 5e3, True),
+    'a_P_d_non': (15.6e1, 1e7, True),        
+    'k_P_d_non':  (1e-3, 10, True),      
+    'r_P_d_non': (4, 15, False),
+    'fac_R_d_non': (1e-16, 1, False),
+    'k_iE_pit_frac_non': (0, 1, False),        # Anteil der iE die durch ART gepitted werden, 0-1. Rest sterben durch ART
     })
     return bounds
 
@@ -94,7 +116,8 @@ def main():
     #data = pd.read_csv(fit_data)
     data = {"pth":pd.read_csv("pth.csv"),       
             "non":pd.read_csv("non_pth.csv")}
-    data_used =["pth.csv","non_pth.csv"]
+    data_used =["pth.csv",
+                "non_pth.csv"]
 
     est_obj = FitManager(model, data)    
     ParamEster = ParameterEstimator()
@@ -111,8 +134,9 @@ def main():
         pre_t = est_obj.default_pre_t
     
     update_parameters={} 
+    keys = ['pth','non']
     for key in data.keys():
-        usedpars = {k.replace("_"+key,""):v for k,v in best_parameters.items() if key in k or not k.split('_')[-1] in data.keys()}
+        usedpars = {k.replace("_"+key,""):v for k,v in best_parameters.items() if key in k or not k.split('_')[-1] in keys}
         dummy_dict = get_steady_state(model, usedpars)
         update_parameters[key] = {k:v for k,v in dummy_dict.items() }
 

45590b7267eadc875d019cd06ee32f522473a7ef
nexia23 <karnetzki.maxim@gmx.de>
2024-03-05 09:45:37 +0100

    added parameter model name as bounds function differs between the model ideas also in estimation.py

--- Estimation/run_estimation.py
+++ Estimation/run_estimation.py
@@ -8,62 +8,100 @@ import time
 import os
 import json
 
-def get_params_bounds():
+def get_params_bounds(model_name):
     bounds = OrderedDict({
-    #'Hkt_init': (0.35, 0.55, False),   
-    'k_E_infect': (7e-7 , 4.5e-5, True),    # jetzt paramscan früher (1e-8 , 1e-4),
-    'tropism': (2, 200, False),
-    'M':  (1e2, 5e3, True),
-    'a_P_d': (15.6e1, 1e7, True),        
-    'k_P_d':  (1e-3, 10, True),      
-    'r_P_d': (1, 15, False),
-    'fac_R_d': (1e-16, 1, False),
-    'k_P_art_max': (1e-9, 1e1, True),
-    't_mat_P': (5, 14, False),
-    'slope_rpi':(1e0, 500, True),            # own idea
-    #'k_M_death': (60, 100, False),
-    #'t_E_death': (100, 130, False),
-    'k_iE_pit_frac': (0, 1, False),         # Anteil der iE die durch ART gepitted werden, 0-1. Rest sterben durch ART
-    #'k_iE_art_max': (1, 1000),             # maximale abtötrate von iE durch ART, #parameterscan zu unsensibel 
-    #'h_art' :(3, 6, False),                # assume similar to in vitro: [1] R. K. Tyagi u. a., doi: 10.1186/s12916-018-1156-x.
-    #'ID50': (1e-1, 1000),                  # ART dosis bei der 50% der iE getötet werden, #parameterscan zu unsensibel       
-    #'pre_t': (2,6, True),                  # time of ART addition, 3 and 5 in medians in data for non-pth and pth respectively
-    
-    # Pth specific parameteres
+        #'Hkt_init': (0.35, 0.55, False),   
+        'k_E_infect': (7e-7 , 4.5e-5, True),    # jetzt paramscan früher (1e-8 , 1e-4),
+        'tropism': (2, 200, False),
+        'M':  (1e2, 5e3, True),
+        'a_P_d': (15.6e1, 1e7, True),        
+        'k_P_d':  (1e-3, 10, True),      
+        'r_P_d': (1, 15, False),
+        'fac_R_d': (1e-16, 1, False),
+        'k_P_art_max': (1e-9, 1e1, True),
+        't_mat_P': (5, 14, False),
+        'slope_rpi':(1e0, 500, True),            # own idea
+        #'k_M_death': (60, 100, False),
+        #'t_E_death': (100, 130, False),
+        'k_iE_pit_frac': (0, 1, False),         # Anteil der iE die durch ART gepitted werden, 0-1. Rest sterben durch ART
+        #'k_iE_art_max': (1, 1000),             # maximale abtötrate von iE durch ART, #parameterscan zu unsensibel 
+        #'h_art' :(3, 6, False),                # assume similar to in vitro: [1] R. K. Tyagi u. a., doi: 10.1186/s12916-018-1156-x.
+        #'ID50': (1e-1, 1000),                  # ART dosis bei der 50% der iE getötet werden, #parameterscan zu unsensibel       
+        #'pre_t': (2,6, True),                  # time of ART addition, 3 and 5 in medians in data for non-pth and pth respectively
+        
+        # Pth specific parameteres
+        #'Hkt_init_pth': (0.35, 0.55, False),   
+        #'t_mat_P_pth': (5,14, False),
+        #'tropism_pth': (2, 200, False),
+        #'k_P_art_max_pth': (1e-9, 1e1, True),
+        't_E_death_pth': (100, 130, False),
+        's_BH_pth': (1e-8, 1e-4, True),         # slope of linear function defining bystander heamolysis strength
+        'LDH_pth': (140, 280, False),           # LDH concentration in blood plasma
+        'k_M_death_pth': (30, 100, False),
+        #'M_pth':  (1e2, 5e3, True),
+        #'a_P_d_pth': (15.6e1, 1e7, True),        
+        #'k_P_d_pth':  (1e-3, 10, True),      
+        #'r_P_d_pth': (4, 15, False),
+        #'fac_R_d_pth': (1e-16, 1, False),
+        #'k_iE_pit_frac_pth': (0, 1, False),        # Anteil der iE die durch ART gepitted werden, 0-1. Rest sterben durch ART
 
-    #'Hkt_init_pth': (0.35, 0.55, False),   
-    #'t_mat_P_pth': (5,14, False),
-    #'tropism_pth': (2, 200, False),
-    #'k_P_art_max_pth': (1e-9, 1e1, True),
-    't_E_death_pth': (100, 130, False),
-    's_BH_pth': (1e-9, 1e-5, True),         # slope of linear function defining bystander heamolysis strength
-    'LDH_pth': (140, 280, False),           # LDH concentration in blood plasma
-    'k_M_death_pth': (30, 100, False),
-    #'M_pth':  (1e2, 5e3, True),
-    #'a_P_d_pth': (15.6e1, 1e7, True),        
-    #'k_P_d_pth':  (1e-3, 10, True),      
-    #'r_P_d_pth': (4, 15, False),
-    #'fac_R_d_pth': (1e-16, 1, False),
-    #'k_iE_pit_frac_pth': (0, 1, False),        # Anteil der iE die durch ART gepitted werden, 0-1. Rest sterben durch ART
-
-    # non-Pth specific parameteres
-
-    #'Hkt_init_non': (0.35, 0.55, False),  
-    #'t_mat_P_non': (5,14, False),
-    #'tropism_non': (2, 200, False),
-    #'k_P_art_max_non': (1e-9, 1e1, True),
-    't_E_death_non': (100, 130, False),
-    's_BH_non': (1e-11, 1e-8, True),      # slope of linear function defining bystander heamolysis strength
-    'LDH_non': (140, 280, False),       # LDH concentration in blood plasma
-    'k_M_death_non': (30, 100, False),
-    #'t_E_death_inf_non': (40, 130, False),
-    #'M_non':  (1e2, 5e3, True),
-    #'a_P_d_non': (15.6e1, 1e7, True),        
-    #'k_P_d_non':  (1e-3, 10, True),      
-    #'r_P_d_non': (4, 15, False),
-    #'fac_R_d_non': (1e-16, 1, False),
-    #'k_iE_pit_frac_non': (0, 1, False),        # Anteil der iE die durch ART gepitted werden, 0-1. Rest sterben durch ART
-    })
+        # non-Pth specific parameteres
+        #'Hkt_init_non': (0.35, 0.55, False),  
+        #'t_mat_P_non': (5,14, False),
+        #'tropism_non': (2, 200, False),
+        #'k_P_art_max_non': (1e-9, 1e1, True),
+        't_E_death_non': (100, 130, False),
+        's_BH_non': (1e-11, 1e-5, True),      # slope of linear function defining bystander heamolysis strength
+        'LDH_non': (140, 280, False),       # LDH concentration in blood plasma
+        'k_M_death_non': (30, 100, False),
+        #'t_E_death_inf_non': (40, 130, False),
+        #'M_non':  (1e2, 5e3, True),
+        #'a_P_d_non': (15.6e1, 1e7, True),        
+        #'k_P_d_non':  (1e-3, 10, True),      
+        #'r_P_d_non': (4, 15, False),
+        #'fac_R_d_non': (1e-16, 1, False),
+        #'k_iE_pit_frac_non': (0, 1, False),        # Anteil der iE die durch ART gepitted werden, 0-1. Rest sterben durch ART
+        })
+    extra_bounds = {}
+    if model_name == 'Hapto':
+        extra_bounds = {
+            # Pth specific parameteres
+            'par1_fHb_pth': (4e3, 6e3, False),
+            'par2_fHb_pth': (4e-4, 4e-3, False),
+            'switch_fHb_pth' : (4e3, 8e3, False),
+            # non-Pth specific parameteres
+            'par1_fHb_non': (1e3, 1e4, False),
+            'par2_fHb_non': (1e-4, 1e-2, True),
+            'switch_fHb_non' : (1e0, 6e4, True),
+            }
+    elif model_name=="immune":
+        # delete not used parameters from bounds
+        bounds.pop('s_BH_pth')
+        bounds.pop('s_BH_non')
+        extra_bounds = {
+            'Treg':(1e2,1e3, False),
+            'Ttox':(1e2, 1e3, False),
+            'k_digest_R': (1e-12, 1e0, True), 
+            'beta_Treg': (1e-4, 1e0, True),
+            #'beta_in_Treg':(2e-4, 2e0, True),
+            'delta_Treg':(1e-3, 1e3, True),   # 109
+            'mu_tox':(1e-3,1e2, True),        # 22
+            #'mu_in_tox':(1e0,1e8, True),      # 1e5
+            'V_f':(1e-2, 5e2, True),           # 165
+            'K_f':(1e0, 2e2, True),          # 20
+            'delta_Ttox':(1e-3, 1e2, True),#  = 0.6
+            'epsilon':(1e-6, 2e0, True),#     = 0.8
+            
+            # Pth specific parameteres
+            'Hkt_init_pth': (0.35, 0.55, False),
+            'beta_in_Treg_pth':(2e-4, 2e0, True),
+            'mu_in_tox_pth':(1e0,1e8, True),      # 1e5
+            # non-Pth specific parameteres
+            'Hkt_init_non': (0.35, 0.55, False),
+            'beta_in_Treg_non':(2e-4, 2e0, True),
+            'mu_in_tox_non':(1e0,1e8, True),      # 1e5
+            }
+    bounds.update(extra_bounds)
     return bounds
 
 def calculate_cma_std(bounds):
@@ -81,7 +119,8 @@ def calculate_cma_std(bounds):
     
 
 def save_estimation(best_score, best_parameters, update_params, ParamEster, fit_data:str, 
-                    bounds: OrderedDict, run_id:int):
+                    bounds: OrderedDict, run_id:int, location: str = 'general'):
+    
     keys = update_params.keys()
     for key in update_params:
         best_pars = {k.replace("_"+key,""):v for k,v in best_parameters.items() if key in k or not k.split('_')[-1] in keys}
@@ -103,31 +142,31 @@ def save_estimation(best_score, best_parameters, update_params, ParamEster, fit_
         save_to = key
         if key == "non": save_to ="non_pth"
         
-        os.makedirs(f'{save_to}', exist_ok=True)
+        os.makedirs(f'{location}/{save_to}', exist_ok=True)
 
-        with open(f'{save_to}/{name}_{run_id}.json', "w") as write_file:
+        with open(f'{location}/{save_to}/{name}_{run_id}.json', "w") as write_file:
             json.dump(result_dict, write_file, indent=4)
 
 
 def main():
-    # fit_data = sys.argv[1]
-    run_id = sys.argv[1]
-    model = te.loada('../LCT_model/LCT_OIE.ant')
-
+    model_name = sys.argv[1]
+    run_id = sys.argv[2]
+    model = te.loada(f'../LCT_model/{model_name}_LCT_OIE.ant')
     #data = pd.read_csv(fit_data)
     data = {"pth":pd.read_csv("pth.csv"),       
             "non":pd.read_csv("non_pth.csv")}
     data_used =["pth.csv",
                 "non_pth.csv"]
 
-    est_obj = FitManager(model, data)    
+    est_obj = FitManager(model, data, model_name)    
     ParamEster = ParameterEstimator()
-    bounds = get_params_bounds()
+    bounds = get_params_bounds(model_name)
     stds = calculate_cma_std(bounds)
     ParamEster.initialize(est_obj.objective_function, bounds)
     best_score, best_parameters, runtime = ParamEster.run(method='cma', iterations=20,
                                                           run_id=run_id, n_lhs=1,
-                                                          optimizer_args={'CMA_stds': stds})
+                                                          optimizer_args={'CMA_stds': stds,
+                                                                          'popsize': 30})
     # Saving of estimation results
     if 'pre_t' in best_parameters:  
         pre_t = best_parameters.pop('pre_t')
@@ -143,7 +182,7 @@ def main():
 
     best_parameters['pre_t'] = pre_t
     save_estimation(best_score, best_parameters, update_parameters, ParamEster,
-                    data_used, bounds, run_id)
+                    data_used, bounds, run_id, location=model_name)
 
     print(best_score, best_parameters, runtime)
 

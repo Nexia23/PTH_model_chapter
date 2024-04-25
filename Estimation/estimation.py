652cc1f73fe35cdcb9d25bdc8dcaaa7cfea96b17
nexia23 <karnetzki.maxim@gmx.de>
2024-04-25 17:22:34 +0200

    tregs now estimated(1,1000), as number was to high in runs

--- Estimation/estimation.py
+++ Estimation/estimation.py
@@ -42,10 +42,12 @@ def get_steady_state(model, pars: dict, model_name: str ='general'):
     eq_dict = {}
     # Immune response in steady state
     if model_name == 'immune':
-        Treg_init = model.beta_Treg/model.delta_Treg * E_init
+        
+        # Immune parameters
+        beta_Treg = model.Treg * model.delta_Treg / E_init
         # Calc T_tox_init from equation
         T = symbols('T')
-        M = model.mu_tox * E_init / Treg_init # model.mu_tox * model.delta_Treg / model.beta_Treg # 
+        M = model.mu_tox * model.delta_Treg / beta_Treg # model.mu_tox * E_init / model.Treg # 
         A = -model.epsilon
         B = model.V_f - model.delta_Ttox - model.K_f * model.epsilon
         C = M - model.delta_Ttox * model.K_f
@@ -55,16 +57,39 @@ def get_steady_state(model, pars: dict, model_name: str ='general'):
         # Take last value as often only positve and highest value
         Ttox_init = float(list(p)[-1])
 
-        #print(Ttox_init)
-        k_digest_E_init   = model.k_R_aging * R_init/(E_init*Ttox_init) #model.k_E_death / 546.8315756308999
+        # R_init from E_init , t_R_a_init and t_E_death (fixed)
+        R_init = E_init * ( t_R_a_init/model.t_E_death)
+        k_R_death_init = model.fac_R_d * model.k_digest_R * Ttox_init
+        # equilibrating precursors (P)
+        # P_init from R_init, t_R_a_init and t_P_a_init
+        P_init = (R_init * (k_R_death_init + 2*np.log(2)/t_R_a_init))/ (2**10 *2*np.log(2) / t_P_a_init)    
+       
+        J_P_death_init = P_init * (model.a_P_d / ( 1 + model.k_P_d * Hb_init**model.r_P_d))**(-1)
+        J_P_aging_init = P_init * np.log(2) / (t_P_a_init/2)
+        k_P_birth_init = J_P_death_init + J_P_aging_init
+
+        # Hb_init, needed for J_P_death
+        Hb_init = (model.Vol_E * E_init * model.Hb_conc_E + model.Vol_R * R_init * model.Hb_conc_R) / (10*model.Vol_blood)  # conc unit from g/l to g/dl, thus div. by 10
+
+        # equilibrating LDH
+        k_digest_E_init   = 2*np.log(2)/(model.t_E_death*Ttox_init)#model.k_R_aging * R_init/(E_init*Ttox_init) #model.k_E_death / 546.8315756308999
+        print(2*np.log(2)/(model.t_E_death*Ttox_init))
+        J_LDH_decay_init = model.LDH * (np.log(2) / model.t_halb_LDH_decay)
+        J_E_death_init = E_init * k_digest_E_init * Ttox_init
+
+        J_R_death_init = R_init * k_R_death_init
+        LDH_RBC_init = (J_LDH_decay_init * model.Vol_blood) / (J_E_death_init + J_R_death_init )
+        print(J_R_death_init)
+
         k_digest_oiE_init = model.k_oiE_death/Ttox_init
 
-        eq_dict['Treg']  = Treg_init
-        eq_dict['Ttox']  = Ttox_init 
-        eq_dict['k_digest_E'] = k_digest_E_init
-        eq_dict['k_digest_iE'] = 48 * k_digest_E_init
-        eq_dict['k_digest_M']   = 48 * k_digest_E_init
+        eq_dict['beta_Treg']    = beta_Treg
+        eq_dict['Ttox']         = Ttox_init 
+        eq_dict['k_digest_E']   = k_digest_E_init
+        eq_dict['k_digest_iE']  = 48 * k_digest_E_init
+        #eq_dict['k_digest_M']   = 48 * k_digest_E_init
         eq_dict['k_digest_oiE'] = k_digest_oiE_init
+        eq_dict['k_R_death']    = k_R_death_init
 
     # Hapto in steady state, k_deaths change
     elif model_name == 'Hapto':
@@ -98,7 +123,6 @@ def get_steady_state(model, pars: dict, model_name: str ='general'):
 
     # include chosen parameters
     eq_dict.update(pars)
-
     return eq_dict
 
 def set_model_to_ss(model, pars: dict, model_name: str='general'):
